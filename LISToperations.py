5a7743467abc91359846d05a104425c4ce0f0a62
Nyenzo Isabwa <56804358+Nyenzo@users.noreply.github.com>
2020-01-15 10:29:04 +0300

    Add files via upload
    learnt how to perfom basic list operations(day1)

---
+++ LISToperations.py
@@ -0,0 +1,11 @@
+nyenzo_list='kuku','paper','chapo','honey','drinks','fluor'
+print(nyenzo_list)
+print(nyenzo_list[4])
+nyenzo_list.append('cake')
+print(nyenzo_list)
+del nyenzo_list[1]
+print(nyenzo_list)
+list2='pen','pencil','ruler','rubber','books'
+print(list2)
+shoppinglist=nyenzo_list+list2
+
